52603eaf03ea8ec1a931bd14b349d0acf5470e5d
Vitorhugofsousa <vitorhugofs2018@gmail.com>
2025-02-22 11:04:03 -0300

    adicionando bibliotecas necessárias e configurando pinagem

--- esp.c
+++ esp.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"     
+#include "lib/font.h"
+#include "lib/ssd1306.h"
+#include "hardware/pio.h"
+#include "pico/bootrom.h"
+
+#define BOTAO_A 5 //pino saida botao a
+#define BOTAO_B 6 //pino saida botao b
+#define LED_PIN_GREEN 11 //pino saida led verde
+#define LED_PIN_BLUE 12 //pino saida led azul
+#define LED_PIN_RED 13  //pino saida led vermelho
+#define NUM_PIXELS 25 // número de leds na matriz
+#define matriz 7    //matriz de leds
+#define I2C_SDA 14 //pino SDA
+#define I2C_SCL 15 //pino SCL
+#define SW_PIN 22 //pino do botão SW JOYSTICK
+#define VRX_PIN 26   //pino do eixo X do joystick
+#define VRY_PIN 27   //pino do eixo Y do joystick
+#define I2C_PORT i2c1 //porta I2C
+#define display_address 0x3C //endereço do display
+#define buzzer 21 //pino buzzer A
+#define microfone 28 //pino do microfone
 
 
 
